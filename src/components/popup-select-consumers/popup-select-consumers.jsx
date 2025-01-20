43d8526afccc1174560659f94dde20488a733e79
Dmitry Antushev <fyneitd@mail.ru>
2025-01-20 14:25:52 +0300

    feat(select-consumers): edit select field on React-select librarry

--- src/components/popup-select-consumers/popup-select-consumers.jsx
+++ src/components/popup-select-consumers/popup-select-consumers.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-
-import Preloader from '../preloader/preloader.jsx';
+import React, {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import { toast } from 'react-toastify';
+import Select from 'react-select';
+import * as dayjs from "dayjs";
 
 import {
   fetchFirmsByIdUser,
-  setFirmsActiveByIdUser,
-  fetchProductsWithSummaryDetail
+  fetchProductsWithSummaryDetail,
+  setFirmsActiveByIdUser
 } from '../../store/api-actions.js';
 import {
   getCurrentUser,
@@ -14,29 +15,51 @@ import {
   getStatusLoadProducts
 } from '../../store/app-data/selectors.js';
 
-import * as dayjs from "dayjs";
+import Preloader from '../preloader/preloader.jsx';
+
+const TypeShowConditionValue = {
+  VALUE: 'value',
+  PERCENT: 'percent'
+}
 
 const PopupSelectConsumers = (props) => {
   const dispatch = useDispatch();
 
-  const { firms: propFirms, currentProduct, setIsOpen } = props;
+  const { firms, currentProduct, setIsOpen } = props;
   const currentUser = useSelector(getCurrentUser);
   const isLoadFirms = useSelector(getStatusLoadFirms);
   const isLoadProducts = useSelector(getStatusLoadProducts);
 
-  const [firms, setFirms] = useState(propFirms);
+  const getFirmFormatDataForSelect = (firms) => {
+    return firms.map((firm) => {
+      return {
+        label: firm.name,
+        value: firm
+      }
+    });
+  }
+
+  const [firmsSelectOptions, setFirmsSelectOptions] = useState(getFirmFormatDataForSelect(firms));
+  const [firmsSelect, setFirmsSelect] = useState(getFirmFormatDataForSelect(firms.filter((firm => firm.isSelect))));
+  const [typeShowConditionValue, setTypeShowConditionValue] = useState(TypeShowConditionValue.VALUE);
+
+  const getValidateStatus = (firmsSelect) => {
+   return firmsSelect.some((firm) => firm.value.isMain === true);
+  }
+  const getMainFirm = (firms) => firms?.find((firm) => firm.isMain);
+  const getFirmsFormatDataForWriteDb = (firmsSelect) => firmsSelect.map((firm) => firm.value);
 
   return (
     <>
       <div className="background-black" />
-      <div className="modal">
+      <div className="modal modal--settings-monitoring-table">
         <div
           className="modal__close"
           onClick={() => {
             setIsOpen(false)
           }}
         >
-          <svg width="18" height="18" viewBox="0 0 18 18">
+          <svg className="icon" width="18" height="18" viewBox="0 0 18 18">
             <path
               d="M11.8516 8.59375L16.7378 3.70752C17.3374 3.10791 17.3374 2.13574 16.7378 1.53564L15.6519 0.449707C15.0522 -0.149902 14.0801 -0.149902 13.48 0.449707L8.59375 5.33594L3.70752 0.449707C3.10791 -0.149902 2.13574 -0.149902 1.53564 0.449707L0.449707 1.53564C-0.149902 2.13525 -0.149902 3.10742 0.449707 3.70752L5.33594 8.59375L0.449707 13.48C-0.149902 14.0796 -0.149902 15.0518 0.449707 15.6519L1.53564 16.7378C2.13525 17.3374 3.10791 17.3374 3.70752 16.7378L8.59375 11.8516L13.48 16.7378C14.0796 17.3374 15.0522 17.3374 15.6519 16.7378L16.7378 15.6519C17.3374 15.0522 17.3374 14.0801 16.7378 13.48L11.8516 8.59375Z"
               fill="#BE1622"/>
@@ -44,67 +67,73 @@ const PopupSelectConsumers = (props) => {
         </div>
 
         <h2 className="header header--2 header--center">Настройки таблицы</h2>
-        <p className="modal__text text text--center text--italic">
-          Выберите <b>не более 4</b> конкурентов, которые будут отображаться в таблице.
+        <p className="modal__text">
+          Выберите конкурентов для отображения в таблице
         </p>
 
+        <Select
+          className="input input--select input--select-consumers input--margin-bottom"
+          maxMenuHeight={190}
+          closeMenuOnSelect={false}
+          cacheOptions
+          isMulti={true}
+          value={firmsSelect}
+          options={firmsSelectOptions}
+          placeholder="Выберите одну или несколько компаний"
+          onChange={(evt) => {
+            const mainFirm = getMainFirm(firms);
 
-        <ul className="consumers-list">
-          {
-            firms.map((firm) => {
-              return (
-                <li
-                  key={firm.id}
-                  className="consumers-list__item"
-                  style={getFirmActiveStyles(firm)}
-                  onClick={() => {
-                    setFirms((prevFirms) => {
-                     return prevFirms.map((prevFirm) => {
-                       if (prevFirm.id === firm.id) {
-                         return {...prevFirm, isSelect: !firm.isSelect}
-                       } else {
-                         return prevFirm;
-                       }
-                     })
-                    })
-                  }}
-                  onMouseOver={() => {
-                    setFirms((prevFirms) => {
-                      return prevFirms.map((prevFirm) => {
-                        if (prevFirm.id === firm.id) {
-                          return {...prevFirm, isSelect: !firm.isSelect}
-                        } else {
-                          return prevFirm;
-                        }
-                      })
-                    })
-                  }}
-                  onMouseOut={() => {
-                    setFirms((prevFirms) => {
-                      return prevFirms.map((prevFirm) => {
-                        if (prevFirm.id === firm.id) {
-                          return {...prevFirm, isSelect: !firm.isSelect}
-                        } else {
-                          return prevFirm;
-                        }
-                      })
-                    })
-                  }}
-                >
-                  {firm.name}
-                </li>
-              )
-            })
-          }
-        </ul>
+            if (evt.length === 0) {
+              evt.push({ label: mainFirm.name, value: mainFirm });
+            }
+
+            const isValid = getValidateStatus(evt);
+            if (!isValid) {
+              toast.warning(`Фирма ${mainFirm.name} является основной - из списка выбора её убирать нельзя`);
+
+              return false;
+            }
+
+            setFirmsSelect(evt);
+          }}
+        />
+
+        {/*<p className="modal__text">*/}
+        {/*  Выберите способ расчёта разницы величин*/}
+        {/*</p>*/}
+        {/*<ul className="toggle-data-list toggle-data-list--margin-bottom">*/}
+        {/*  <li*/}
+        {/*    className={`toggle-data-list__item ${*/}
+        {/*      typeShowConditionValue === TypeShowConditionValue.VALUE && 'toggle-data-list__item--active'*/}
+        {/*    }`*/}
+        {/*    }*/}
+        {/*    onClick={() => {*/}
+        {/*      // WIP: ДОДЕЛАТЬ ОБРАБОТЧИКИ РЕДАКТИРОВАНИЯ ОТОБРАЖАЕМЫХ ЗНАЧЕНИЙ*/}
+        {/*    }}*/}
+        {/*  >*/}
+        {/*    Значения*/}
+        {/*  </li>*/}
+        {/*  <li*/}
+        {/*    className={`toggle-data-list__item ${*/}
+        {/*      typeShowConditionValue === TypeShowConditionValue.PERCENT && 'toggle-data-list__item--active'*/}
+        {/*    }`*/}
+        {/*    }*/}
+        {/*    onClick={() => {*/}
+        {/*      // WIP: ДОДЕЛАТЬ ОБРАБОТЧИКИ РЕДАКТИРОВАНИЯ ОТОБРАЖАЕМЫХ ЗНАЧЕНИЙ*/}
+        {/*    }}*/}
+        {/*  >*/}
+        {/*    Проценты*/}
+        {/*  </li>*/}
+        {/*</ul>*/}
 
         <button
           disabled={isLoadProducts}
-          className="button button--add-group"
+          className="button button--add-group button--bottom"
           type="button"
           onClick={async () => {
             await dispatch(setFirmsActiveByIdUser({
-              idUser: currentUser.id, firms
+              idUser: currentUser.id,
+              firms: getFirmsFormatDataForWriteDb(firmsSelect)
             }));
             await dispatch(fetchFirmsByIdUser(currentUser.id));
             await dispatch(fetchProductsWithSummaryDetail({
