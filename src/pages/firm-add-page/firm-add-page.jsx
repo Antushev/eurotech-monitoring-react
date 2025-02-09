8578b610abd6c2b8420de42a833dff153893651e
Dmitry Antushev <fyneitd@mail.ru>
2025-02-09 12:35:17 +0300

    fix(webpack): disabled open browser then project start

--- src/pages/firm-add-page/firm-add-page.jsx
+++ src/pages/firm-add-page/firm-add-page.jsx
@@ -4,13 +4,67 @@ const FirmAddPage = () => {
   return (
     <section className="page-content page__content">
       <header className="page-content__header standart-block">
-        <h1 className="header header--1">Создание триггера</h1>
+        <h1 className="header header--1">Создание фирмы</h1>
       </header>
 
       <section className="page-content__inner">
         <form>
-          <div className="standart-block">
+          <div className="standart-block standart-block--space-bottom-20">
             <h2 className="header header--2 header--space-bottom">Базовые данные</h2>
+
+            <div className="form-block">
+              <label className="label label--margin-right" htmlFor="firm-name">
+                <span className="label__text">Название фирмы</span>
+                <input
+                  id="firm-name"
+                  className="input input--no-margin-bottom"
+                  name="firm-name"
+                  type="text"
+                  placeholder="Например: ООО ЕВРОТЕК"
+                  title="Название фирмы"
+                  alt="Название фирмы"
+                />
+              </label>
+
+              <label className="label label--margin-right" htmlFor="inn">
+                <span className="label__text">ИНН</span>
+                <input
+                  id="inn"
+                  className="input input--no-margin-bottom"
+                  type="text"
+                  title="ИНН добавляемой фирмы"
+                  alt="ИНН добавляемой фирмы"
+                />
+              </label>
+
+              <label className="label label--margin-right" htmlFor="kpp">
+                <span className="label__text">КПП</span>
+                <input
+                  id="kpp"
+                  className="input input--no-margin-bottom"
+                  name="kpp"
+                  type="text"
+                  title="КПП добавляемой фирмы"
+                  alt="КПП добавляемой фирмы"
+                />
+              </label>
+
+              <label className="label" htmlFor="color">
+                <span className="label__text">Цвет</span>
+                <input
+                  id="color"
+                  className="input input--color input--no-margin-bottom"
+                  name="color"
+                  type="color"
+                  title="Цвет для фирмы"
+                  alt="Цвет для фирмы"
+                />
+              </label>
+            </div>
+          </div>
+
+          <div className="standart-block standart-block--space-bottom-20">
+            <h2 className="header header--2 header--space-bottom">Настройка интеграции с сайтом</h2>
           </div>
         </form>
       </section>
--- src/pages/trigger-edit-page/trigger-edit-page.jsx
+++ src/pages/trigger-edit-page/trigger-edit-page.jsx
@@ -223,8 +223,8 @@ const TriggerAddPage = () => {
             <h2 className="header header--2 header--space-bottom">Шаг 1. Базовые данные</h2>
 
             <div className="form-block">
-              <label className="trigger-add__input" for="trigger-name">
-                <span className="input__label-text">Название триггера</span>
+              <label className="label label--margin-right" for="trigger-name">
+                <span className="label__text">Название триггера</span>
                 <input
                   ref={nameTriggerRef}
                   id="name-trigger"
@@ -245,8 +245,8 @@ const TriggerAddPage = () => {
                 />
               </label>
 
-              <label className="trigger-add__input">
-                <span className="input__label-text">Тип срабатывания</span>
+              <label className="label label--margin-right">
+                <span className="label__text">Тип срабатывания</span>
                 <select
                   id="type-active"
                   className="input input--trigger"
@@ -268,7 +268,7 @@ const TriggerAddPage = () => {
               {
                 typeActive === 'IS_ACTIVE' &&
                 <label className="input--margin-right">
-                  <span className="input__label-text">Интервал срабатывания</span>
+                  <span className="label__text">Интервал срабатывания</span>
                   <select
                     id="active-interval"
                     className="input input--trigger input--margin-right"
@@ -294,7 +294,7 @@ const TriggerAddPage = () => {
               {
                 typeActive === 'IS_ACTIVE' &&
                 <label>
-                  <span className="input__label-text">Количество срабатываний</span>
+                  <span className="label__text">Количество срабатываний</span>
                   <input
                     id="active-count"
                     className="input input--trigger"
@@ -311,7 +311,7 @@ const TriggerAddPage = () => {
 
             <div className="form-block form-block--margin-bottom">
               <label>
-                <span className="input__label-text">Период действия</span>
+                <span className="label__text">Период действия</span>
                 <div className="date-select">
                   <input
                     className="date-select__input date-select__from"
